4de8ec6af40a5d34e42f7ac872481cea10534505
LiamBB <liam@eastcode.ca>
2023-05-14 23:58:25 -0300

    Implemented an electrs TCP interface

---
+++ electrsInterface.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+)
+
+func Electrsinterface(method string, params []interface{}) string {
+	// Establish a TCP connection to the server
+	conn, err := net.Dial("tcp", "127.0.0.1:50001")
+	if err != nil {
+		return ""
+	}
+	defer conn.Close()
+	data := map[string]interface{}{
+		"method": method,
+		"params": params,
+		"id":     0,
+	}
+
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return ""
+	}
+	// Send the request message to the server
+	_, err = fmt.Fprintf(conn, "%s\n", payload)
+	if err != nil {
+		return ""
+	}
+
+	// Read the response message from the server
+	responseBytes, err := bufio.NewReader(conn).ReadBytes('\n')
+	if err != nil {
+		return ""
+	}
+	return string(responseBytes)
+}
